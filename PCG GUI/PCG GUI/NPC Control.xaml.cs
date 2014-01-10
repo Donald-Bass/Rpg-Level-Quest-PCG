ffa4e967f25688300d1aa1ade80b8e01561b585a
Donald <dbass@ccs.neu.edu>
2014-01-10 14:54:29 -0500

    Started work on adding rooms to level generation

---
+++ PCG GUI/PCG GUI/NPC Control.xaml.cs
@@ -0,0 +1,31 @@
+﻿using PCG_GUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PCG_GUI
+{
+    /// <summary>
+    /// Interaction logic for NPC_Control.xaml
+    /// </summary>
+    public partial class NPC_Control : UserControl
+    {
+        public ViewModelParent viewModel { get; set;  }
+
+        public NPC_Control()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PCG GUI/PCG GUI/SmartObservableCollection.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Code for the moment borrowed from http://stackoverflow.com/questions/13302933/how-to-avoid-firing-observablecollection-collectionchanged-multiple-times-when-r
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCG_GUI
+{
+    public class SmartObservableCollection<T> : ObservableCollection<T>
+    {
+        public SmartObservableCollection()
+            : base()
+        {
+        }
+
+        public SmartObservableCollection(IEnumerable<T> collection)
+            : base(collection)
+        {
+        }
+
+        public SmartObservableCollection(List<T> list)
+            : base(list)
+        {
+        }
+
+        public void AddRange(IEnumerable<T> range)
+        {
+            foreach (var item in range)
+            {
+                Items.Add(item);
+            }
+
+            this.OnPropertyChanged(new PropertyChangedEventArgs("Count"));
+            this.OnPropertyChanged(new PropertyChangedEventArgs("Item[]"));
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        public void Reset(IEnumerable<T> range)
+        {
+            this.Items.Clear();
+
+            AddRange(range);
+        }
+    }
+    
+}
