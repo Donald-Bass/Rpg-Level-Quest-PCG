6fee14d944a4d255688af970358008830245e9a8
Donald <dbass@ccs.neu.edu>
2014-12-05 13:35:15 -0500

    Added even more documentation, and a zip file of all the papers I've looked at

--- clingoSpeedTest.bat
+++ clingoSpeedTest.bat
@@ -1,163 +1,17 @@
 @ECHO OFF
+REM This file is an example of how to run experiments with Clingo to help determine the average runtime, in case you are running into performance problems and want to know whether certain changes 
+REM actually help with said problems. Since there is an element of randomness with Clingo any individual run doesn't give you useful information. You need to average the times of a number of runs
+REM to get a better idea of what the actual performance is. How many depends on how small of a change you want to detect. If you make a change that has a huge performance implication its often obvious
+REM after only 5-6 runs. I usually do 30 runthroughs which seems to be more then sufficient to detect most performance changes
+REM This bat file will save the time taken for each runthrough to a file, although currently the user needs to manually find the average themselves.
 
-
-REM for /l %%i in (16,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\WorldDef.txt" --seed=%%i --rand-freq .1 > results.pcg
-	
-	REM more +10 results.pcg >> timingNoOpt.txt
-
-REM )
-
-REM for /l %%i in (16,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCGNonEdge.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\WorldDef.txt" --seed=%%i --rand-freq .1 > results.pcg
-	
-	REM more +10 results.pcg >> timingNoEdge.txt
-
-REM )
-REM for /l %%i in (16,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCGStartRoom.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\WorldDef.txt" --seed=%%i --rand-freq .1 > results.pcg
-	
-	REM more +10 results.pcg >> timingStartRoom.txt
-
-REM )
-
-REM for /l %%i in (16,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\WorldDefWithRoom.txt" --seed=%%i --rand-freq .1 > results.pcg
-	
-	REM more +10 results.pcg >> timingOneRoom.txt
-
-REM )
-
-REM for /l %%i in (16,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCGNonEdge.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\WorldDefWithRoom.txt" --seed=%%i --rand-freq .1 > results.pcg
-	
-	REM more +10 results.pcg >> timingNoEdgeAndRoom.txt
-
-REM )
-
-REM for /l %%i in (1,1,30) do (
-REM	Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDef2010.txt" --seed=%%i --rand-freq .15 > results.pcg
-	
-REM	more +10 results.pcg >> timingDist20by20.txt
-
-REM )
-
-
-REM for /l %%i in (16,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDef4.txt" --seed=%%i --rand-freq .15 > results.pcg
-	
-	REM more +10 results.pcg >> timing4.txt
-
-REM )
-
-REM for /l %%i in (1,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDef5.txt" --seed=%%i --rand-freq .15 > results.pcg
-	
-	REM more +10 results.pcg >> timing5.txt
-
-REM )
-
-REM for /l %%i in (1,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDef6.txt" --seed=%%i --rand-freq .15 > results.pcg
-	
-	REM more +10 results.pcg >> timing6.txt
-
-REM )
-
-REM for /l %%i in (1,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDef7.txt" --seed=%%i --rand-freq .15 > results.pcg
-	
-	REM more +10 results.pcg >> timing7.txt
-
-REM )
-
-REM for /l %%i in (1,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDef8.txt" --seed=%%i --rand-freq .15 > results.pcg
-	
-	REM more +10 results.pcg >> timing8.txt
-
-REM )
-
-REM for /l %%i in (1,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDef9.txt" --seed=%%i --rand-freq .15 > results.pcg
-	
-	REM more +10 results.pcg >> timing9.txt
-
-REM )
-
-REM for /l %%i in (1,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDef10.txt" --seed=%%i --rand-freq .15 > results.pcg
-	
-	REM more +10 results.pcg >> timing10.txt
-
-REM )
-
+REM Loop through 30 times. The first 1 is the starting value for i, the second is how much to increase it by, and the third is the highest value of i to use
 for /l %%i in (1,1,30) do (
 	Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDef.txt" --seed=%%i --rand-freq .05 > results.pcg
 	
 	more +10 results.pcg >> timingWithDistLim.txt
 
 )
-
-
-REM for /l %%i in (1,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCGOld.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDef.txt" --seed=%%i --rand-freq .01 > results.pcg
-	
-	REM more +10 results.pcg >> timing4BranchOld.txt
-
-REM )
-
-REM for /l %%i in (1,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDef.txt" --seed=%%i --rand-freq .05 > results.pcg
-	
-	REM more +10 results.pcg >> timingRand05.txt
-
-REM )
-
-REM for /l %%i in (1,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDef.txt" --seed=%%i --rand-freq .10 > results.pcg
-	
-	REM more +10 results.pcg >> timingRand10.txt
-
-REM )
-
-REM for /l %%i in (1,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDef.txt" --seed=%%i --rand-freq .15 > results.pcg
-	
-	REM more +10 results.pcg >> timingRand15.txt
-
-REM )
-
-REM for /l %%i in (1,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDef.txt" --seed=%%i --rand-freq .20 > results.pcg
-	
-	REM more +10 results.pcg >> timingRand20.txt
-
-REM )
-
-REM for /l %%i in (1,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDef.txt" --seed=%%i --rand-freq .25 > results.pcg
-	
-	REM more +10 results.pcg >> timingRand25.txt
-
-REM )
-
-REM for /l %%i in (1,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDef.txt" > results.pcg
-	
-	REM more +10 results.pcg >> timingNoRand.txt
-
-REM )
-
-REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDefComplex.txt" > resultsC.pcg
-
-REM for /l %%i in (1,1,30) do (
-	REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\WorldDef20by20.txt" --seed=%%i --rand-freq .15 > results.pcg
-	
-	REM more +10 results.pcg >> timingStartAndEnd20.txt
-
-REM )
-
 
 
 PAUSE
--- runClingo.bat
+++ runClingo.bat
@@ -1,5 +1,21 @@
 @ECHO OFF
+REM This is a very simple bat file to run clingo on a windows machine. (The parameters clingo uses in othr OS's should be the same, but the rest will probally have to be rewritten)
+REM The "@ECHO OFF" line stops windows from outputing all the commands entered by this batch file to the console
+
 set seed=%RANDOM%
-REM Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\WorldDef.txt" --seed=%seed% --rand-freq .15 > results.pcg
+				  REM Clingo will always output the same result unless we give it a parameter to add a bit of randomness to the process. However clingo by default will always use the same seed so we need to generate a random
+				  REM seed to give clingo to use
+
+
 Clingo.exe "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG_Resources\PCG.txt" "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\PCG GUI\PCG GUI\bin\Debug\TempWorldDef.txt" --seed=%seed% --rand-freq .01 > "C:\Users\Donald Bass\Documents\Rpg-Level-Quest-PCG\GraphVisCode\results.pcg"
-PAUSE                                                                                                                      
+
+																						REM This line actually runs clingo. 
+																						REM The PCG.txt and TempWorldDef.txt are the input files. 
+																						REM --seed=%seed% sets the seed to the randomly generated seed we just found
+																						REM --rand-freq .15 is the parameter that determines how frequently Clingo will try to make random decisions instead of using its usual heuristics.
+																						REM		This parameter can be kept rather low and still generate varied results.  Its hard to determine it's effect on speed though. I've had clingo code that
+																						REM     has run faster when increasing randomness and other code that has run slower when increasing it
+																						REM > results.pcg Outputs the Results Clingo generates to the file results.pcg
+																						REM		The .pcg extension doesn't really mean anything. It's just a text file, but giving it a unique extension helps make it stand out
+																						
+PAUSE REM The pause prints a message and leaves the console window open until you press a key once clingo finishes running. I find its easier to notice when Clingo is done this way then by having the console close automatically.                                                                                                                     
